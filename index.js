1a89526a9bb9ad79bfb702fdd4b296496242001b
Brian <mahuibrian14@gmail.com>
2023-03-22 14:05:06 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+function multiply(num1,num2){
+    return (num1*num2);
+}
+//console.log(multiply(2,31));
+function random(){
+    return Math.round( (Math.random()*10)+1);
+}
+//console.log(Number.isInteger(random()));
+function mod(num3,num4) {
+   return (num3%num4);
+}
+//console.log(mod(10,6));
+function max(){
+    return Math.max(2,3,4,7,9,14,20);
+}
+
